7dc89b29b120e7a0cd881a11de9d563d5949b0e5
heneche <kirakosyan.lilia@student.rau.am>
2025-03-14 16:24:24 +0400

    added project files

--- docs/info.md
+++ docs/info.md
@@ -9,12 +9,14 @@ You can also include images in this folder and reference them in the markdown. E
 
 ## How it works
 
-TODO: Explain how your project works
+It uses bidirectional movement for objects, by using parameter and change sign of that value.
 
 ## How to test
 
-TODO: Explain how to use your project
+
 
 ## External hardware
 
-TODO: List external hardware used in your project (e.g. PMOD, LED display, etc), if any
+Tang nano 9k
+LCD - 480 * 272 px
+Segment display - TM1638
--- src/hackathon_top.sv
+++ src/hackathon_top.sv
@@ -1,6 +1,3 @@
-// Board configuration: tang_nano_9k_lcd_480_272_tm1638_hackathon
-// This module uses few parameterization and relaxed typing rules
-
 module hackathon_top
 (
     input  logic       clock,
@@ -10,261 +7,186 @@ module hackathon_top
     input  logic [7:0] key,
     output logic [7:0] led,
 
-    // A dynamic seven-segment display
-
     output logic [7:0] abcdefgh,
     output logic [7:0] digit,
-
-    // LCD screen interface
 
     input  logic [8:0] x,
     input  logic [8:0] y,
 
     output logic [4:0] red,
     output logic [5:0] green,
-    output logic [4:0] blue
+    output logic [4:0] blue,
+
+    inout  logic [2:0] gpio
 );
 
-    //------------------------------------------------------------------------
-    //
-    //  Screen, object and color constants
-
+    // Screen, object and color constants
     localparam screen_width  = 480,
                screen_height = 272,
-
                wx            = 30,
                wy            = 30,
-
-               start_0_x     = 0,
-               start_0_y     = screen_height     / 5,
-
-               start_1_x     = screen_width      / 2,
-               start_1_y     = screen_height * 4 / 5,
-
                max_red       = 31,
                max_green     = 63,
                max_blue      = 31;
 
-    //------------------------------------------------------------------------
-    //
-    //  Pulse generator, 50 times a second
-
+    // Pulse generator, 50 times a second
     logic enable;
-
     strobe_gen # (.clk_mhz (27), .strobe_hz (100))
     i_strobe_gen (clock, reset, enable);
 
-    //------------------------------------------------------------------------
-    //
-    //  Finite State Machine (FSM) for the game
-
-    localparam [2:0]
-        STATE_START = 0,
-        STATE_AIM   = 1,
-        STATE_SHOOT = 2,
-        STATE_WON   = 3,
-        STATE_LOST  = 4;
-
+    // FSM state transition logic
     logic [2:0] state, new_state;
-
-    //------------------------------------------------------------------------
-
-    // Conditions to change the state - declarations
-
     logic out_of_screen, collision, launch, timeout;
 
-    //------------------------------------------------------------------------
+    // Object coordinates
+    logic [8:0] x_blue[5:0], y_blue[5:0];
+    logic [8:0] x_red, y_red;
+    logic [8:0] x_green[8:0], y_green[8:0];  // New 9 green objects (8:0 index)
+    logic direction_blue[5:0], direction_green[8:0];  // Directions for green
 
-    always_comb
-    begin
-        new_state = state;
-
-        case (state)
-
-        STATE_START : new_state =                   STATE_AIM;
-
-        STATE_AIM   : new_state =   out_of_screen ? STATE_LOST
-                                  : collision     ? STATE_WON
-                                  : launch        ? STATE_SHOOT
-                                  : timeout       ? STATE_SHOOT
-                                  :                 STATE_AIM;
-
-        STATE_SHOOT : new_state =   out_of_screen ? STATE_LOST
-                                  : collision     ? STATE_WON
-                                  :                 STATE_SHOOT;
-
-        STATE_WON   : new_state =   timeout       ? STATE_START
-                                  :                 STATE_WON;
-
-        STATE_LOST  : new_state =   timeout       ? STATE_START
-                                  :                 STATE_LOST;
-
-        endcase
-    end
-
-    //------------------------------------------------------------------------
-
+    // Initialize object positions
     always_ff @ (posedge clock)
-        if (reset)
-            state <= STATE_START;
-        else if (enable)
-            state <= new_state;
-
-    //------------------------------------------------------------------------
-    //
-    //  Computing new object coordinates
-
-    logic [8:0] x0,  y0,  x1,  y1,
-                x0r, y0r, x1r, y1r;
-
-    wire left  = | key [6:1];
-    wire right =   key [0];
-
-    always_comb
     begin
-        x0 = x0r;
-        y0 = y0r;
-        x1 = x1r;
-        y1 = y1r;
-
-        if (state == STATE_START)
+        if (reset)
         begin
-            x0 = start_0_x;
-            y0 = start_0_y;
-            x1 = start_1_x;
-            y1 = start_1_y;
-        end
-        else
-        begin
-            x0 = x0 + 1;
-
-            if (state == STATE_SHOOT)
+            for (int i = 0; i < 6; i++)
             begin
-                x1 = x1 + right - left;
-                y1 = y1 - 1;
+                x_blue[i] <= i * 70 + 5;
+                y_blue[i] <= screen_height / 3 * (i % 2) + wy;
+                x_green[i] <= x_blue[i]; // Initialize green objects at the same position as blue
+                y_green[i] <= y_blue[i];
+                direction_blue[i] <= 1; // Move right
+                direction_green[i] <= 1; // Move down (perpendicular to blue)
             end
+
+            // Initialize remaining 3 green objects
+            for (int i = 6; i < 9; i++)
+            begin
+                x_green[i] <= i * 70 + 5;
+                y_green[i] <= screen_height / 3 * (i % 2) + wy;
+                direction_green[i] <= 1; // Move down initially
+            end
+
+            x_red <= screen_width / 2;
+            y_red <= screen_height / 2;
+        end
+        else if (enable)
+        begin
+            for (int i = 0; i < 6; i++)
+            begin
+                // Blue objects move horizontally
+                if (direction_blue[i] == 1)
+                begin
+                    if (x_blue[i] < screen_width - wx - 5)
+                        x_blue[i] <= x_blue[i] + 1;
+                    else
+                        direction_blue[i] <= 0; // Change direction to left
+                end
+                else
+                begin
+                    if (x_blue[i] > 5)
+                        x_blue[i] <= x_blue[i] - 1;
+                    else
+                        direction_blue[i] <= 1; // Change direction to right
+                end
+
+                // Green objects move vertically (perpendicular to blue objects)
+                if (direction_green[i] == 1)
+                begin
+                    if (y_green[i] < screen_height - wy - 5)
+                        y_green[i] <= y_green[i] + 1; // Move down
+                    else
+                        direction_green[i] <= 0; // Change direction to up
+                end
+                else
+                begin
+                    if (y_green[i] > 5)
+                        y_green[i] <= y_green[i] - 1; // Move up
+                    else
+                        direction_green[i] <= 1; // Change direction to down
+                end
+            end
+
+            // Handle movement for remaining 3 green objects
+            for (int i = 6; i < 9; i++)
+            begin
+                if (direction_green[i] == 1)
+                begin
+                    if (y_green[i] < screen_height - wy - 5)
+                        y_green[i] <= y_green[i] + 1; // Move down
+                    else
+                        direction_green[i] <= 0; // Change direction to up
+                end
+                else
+                begin
+                    if (y_green[i] > 5)
+                        y_green[i] <= y_green[i] - 1; // Move up
+                    else
+                        direction_green[i] <= 1; // Change direction to down
+                end
+            end
+
+            // Red object movement controlled by player
+            if (key[0] && x_red < screen_width - wx - 5) x_red <= x_red + 1;
+            if (key[1] && x_red > 5) x_red <= x_red - 1;
+            if (key[2] && y_red > 5) y_red <= y_red - 1;
+            if (key[3] && y_red < screen_height - wy - 5) y_red <= y_red + 1;
+
+            // Check for collisions with green and blue objects
+for (int i = 0; i < 9; i++)
+begin
+    if (x_red < x_green[i] + wx && x_red + wx > x_green[i] &&
+        y_red < y_green[i] + wy && y_red + wy > y_green[i])
+    begin
+        // Reset red object position if collision occurs
+        x_red <= screen_width / 2;
+        y_red <= screen_height / 2;
+    end
+end
+
+for (int i = 0; i < 6; i++)
+begin
+    if (x_red < x_blue[i] + wx && x_red + wx > x_blue[i] &&
+        y_red < y_blue[i] + wy && y_red + wy > y_blue[i])
+    begin
+        // Reset red object position if collision occurs
+        x_red <= screen_width / 2;
+        y_red <= screen_height / 2;
+    end
+end
         end
     end
 
-    //------------------------------------------------------------------------
-    //
-    //  Updating object coordinates
-
-    always_ff @ (posedge clock)
-        if (reset)
-        begin
-            x0r <= 0;
-            y0r <= 0;
-            x1r <= 0;
-            y1r <= 0;
-        end
-        else if (enable)
-        begin
-            x0r <= x0;
-            y0r <= y0;
-            x1r <= x1;
-            y1r <= y1;
-        end
-
-    //------------------------------------------------------------------------
-    //
-    // Conditions to change the state - implementations
-
-    assign out_of_screen =   x0 == screen_width
-                           | x1 == 0
-                           | x1 == screen_width
-                           | y1 == 0;
-
-    assign collision = ~ (  x0 + wx <= x1
-                          | x1 + wx <= x0
-                          | y0 + wy <= y1
-                          | y1 + wy <= y0 );
-
-    assign launch = left | right;
-
-    //------------------------------------------------------------------------
-    //
-    // Timeout condition
-
-    logic [7:0] timer;
-
-    always_ff @ (posedge clock)
-        if (reset)
-            timer <= 0;
-        else if (state == STATE_START)
-            timer <= 200;
-        else if (state == STATE_SHOOT)
-            timer <= 100;
-        else if (enable)
-            timer <= timer - 1;
-
-    assign timeout = (timer == 0);
-
-    //------------------------------------------------------------------------
-    //
-    //  Determine pixel color
-
-    //------------------------------------------------------------------------
-
+    // Determine pixel color
     always_comb
     begin
         red   = 0;
-        green = 0;
+        green = 0; 
         blue  = 0;
 
-        // verilator lint_off CASEINCOMPLETE
-
-        case (state)
-
-        STATE_WON:
-        begin
-            red = max_red;
-        end
-
-        STATE_LOST:
+        // White border (5 pixels thick)
+        if (x < 5 || x >= screen_width - 5 || y < 5 || y >= screen_height - 5)
         begin
             red   = max_red;
             green = max_green;
+            blue  = max_blue;
         end
-
-        default:
+        else
         begin
-            if (  x >= x0 & x < x0 + wx
-                & y >= y0 & y < y0 + wy)
-            begin
-                blue = max_blue;
-            end
+            // Blue objects
+            for (int i = 0; i < 6; i++)
+                if (x >= x_blue[i] && x < x_blue[i] + wx && y >= y_blue[i] && y < y_blue[i] + wy)
+                    blue = max_blue;
 
-            if (  x >= x1 & x < x1 + wx
-                & y >= y1 & y < y1 + wy)
-            begin
+            // Green objects
+            for (int i = 0; i < 9; i++) // Now checking 9 green objects
+                if (x >= x_green[i] && x < x_green[i] + wx && y >= y_green[i] && y < y_green[i] + wy)
+                    green = max_green;
+
+            // Red object
+            if (x >= x_red && x < x_red + wx && y >= y_red && y < y_red + wy)
                 red = max_red;
-            end
         end
-
-        endcase
-
-        // verilator lint_on CASEINCOMPLETE
     end
-
-    //------------------------------------------------------------------------
-    //
-    //  Output to LED and 7-segment display
-
-    assign led = x1;
-
-    wire [31:0] number
-        = key [7] ? { 7'b0, x0, 7'b0, y0 }
-                  : { 7'b0, x1, 7'b0, y1 };
-
-    seven_segment_display # (.w_digit (8)) i_7segment
-    (
-        .clk      ( clock    ),
-        .rst      ( reset    ),
-        .number   ( number   ),
-        .dots     ( '0       ),  // This syntax means "all 0s in the context"
-        .abcdefgh ( abcdefgh ),
-        .digit    ( digit    )
-    );
 
 endmodule
